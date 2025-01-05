f3c56d57cefdc13df199f7fc28a6eebce14f0ae0
Haleema Tallat <139357574+haleematallat@users.noreply.github.com>
2025-01-05 22:37:17 +0500

    Create Fourier-Transform.py

---
+++ Fourier-Transform.py
@@ -0,0 +1,10 @@
+#Fourier Transform
+
+# F(ω) = ∫[−∞ to ∞] f(t) * e^(-j * ω * t) dt
+#   where:
+#   - F(ω) is the Fourier Transform (frequency domain representation)
+#   - f(t) is the original function (time domain representation)
+#   - ω is the angular frequency (ω = 2πf, f in Hz)
+#   - e^(-j * ω * t) is the complex exponential
+#   - ∫ is the integral over time (t)
+
